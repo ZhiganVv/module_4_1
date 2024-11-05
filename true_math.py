2c8063366015d9449d5f12bee07e242323d474cd
ZhiganVv <zhiganov.vladimir.95@gmail.com>
2024-11-05 11:43:56 +0700

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,15 @@
+# В true_math создайте функцию divide, которая принимает два параметра first и second.
+# Функция должна возвращать результат деления first на second, но когда в second записан 0 - возвращать бесконечность.
+# Бесконечность можно импортировать из встроенной библиотеки math (from math import inf)
+
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first / second
+
+
+
+
+
